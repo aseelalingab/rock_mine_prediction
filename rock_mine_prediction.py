fef189eb843b5057d01815a430f8d8fdbd58b81d
Aseel <ossaali@icloud.com>
2025-09-18 18:15:00 +0800

    My first project

---
+++ rock_mine_prediction.py
@@ -0,0 +1,73 @@
+
+#the important libraries for the project.
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Data collection and data processing
+sonar_data = pd.read_csv("/Users/aseelali/Desktop/Copy of sonar data.csv",header=None)
+print("sonar_data.head",sonar_data.head())
+
+
+# number of rows and columns
+print("sonar_data.shape",sonar_data.shape)
+
+print("sonar_data.describe",sonar_data.describe())
+
+print("sonar_data[60].value_counts",sonar_data[60].value_counts())
+
+
+print("sonar_data.groupby(60).mean()",sonar_data.groupby(60).mean())
+
+
+# separate data and labels
+X = sonar_data.drop(columns=60, axis=1)
+Y = sonar_data[60]
+
+print("the data",X)
+print("the labels",Y)
+
+
+# training and testing data
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.1, stratify=Y, random_state=1)
+print(X.shape,X_train.shape,X_test.shape)
+print(X_train)
+print(Y_train)
+
+# model training and logistic regression
+model= LogisticRegression()
+model.fit(X_train, Y_train)
+
+
+
+#accuracy of training data
+X_train_prediction = model.predict(X_train)
+training_data_accuricy = accuracy_score(X_train_prediction,Y_train)
+print("training_data_accuricy",training_data_accuricy)
+
+#accuracy of testing data
+X_test_prediction = model.predict(X_test)
+testing_data_accuricy = accuracy_score(X_test_prediction,Y_test)
+print("testing_data_accuricy",testing_data_accuricy)
+
+
+#making predictions
+input_data = (0.0200,0.0371,0.0428,0.0207,0.0954,0.0986,0.1539,0.1601,0.3109,0.2111,0.1609,0.1582,0.2238,0.0645,0.0660,0.2273,0.3100,0.2999,0.5078,0.4797,0.5783,0.5071,0.4328,0.5550,0.6711,0.6415,0.7104,0.8080,0.6791,0.3857,0.1307,0.2604,0.5121,0.7547,0.8537,0.8507,0.6692,0.6097,0.4943,0.2744,0.0510,0.2834,0.2825,0.4256,0.2641,0.1386,0.1051,0.1343,0.0383,0.0324,0.0232,0.0027,0.0065,0.0159,0.0072,0.0167,0.0180,0.0084,0.0090,0.0032,)
+#changing the input data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+# reshape the np array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+predictions = model.predict(input_data_reshaped)
+print(predictions)
+
+if (predictions == 'R'):
+    print("this is a Rock")
+else:
+    print("this is a mine  ")
+
+
+
